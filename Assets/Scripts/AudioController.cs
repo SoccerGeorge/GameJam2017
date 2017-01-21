0a2745b4b82dc7c183da6b0dd3ff53c9b3f23cb5
Christopher George <SoccerGeorge10@gmail.com>
2017-01-21 16:09:08 -0600

    Added audio controler and updated moblie control

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioController : MonoBehaviour {
+	public AudioClip[] clips;
+	new AudioSource audio;
+
+	void Awake () {
+		audio = GetComponent<AudioSource>();
+	}
+
+	public void PlayAudio (int index) {
+		audio.clip = clips[index];
+		audio.Play();
+	}
+}
