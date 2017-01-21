d5abf279a99f442d9b999feb129966308dadb6a5
Christopher George <SoccerGeorge10@gmail.com>
2017-01-21 12:25:20 -0600

    Control testing
    Allowed QuitApplication to quit on Android
    Set Project to 2D

--- Assets/MoblieRotate.cs
+++ Assets/MoblieRotate.cs
@@ -1,19 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoblieRotate : MonoBehaviour {
 
+	public float speed = 1.0f;
+	public float speedUpMultiplier = 0.5f;
+	public Text info;
+	Vector3 initPos;
+	Quaternion initRot;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		initPos = transform.position;
+		initRot = transform.rotation;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		if (SystemInfo.supportsGyroscope)
-			transform.rotation = Input.gyro.attitude;
-		else
-			transform.rotation = Quaternion.Euler(Time.time * 10f, 0f, 0f);
+	void Update()
+	{
+		transform.Translate(0f, -(Time.deltaTime * speedUpMultiplier), 0f, Space.World);
+#if UNITY_EDITOR
+		if (Input.GetKey(KeyCode.LeftArrow))
+			transform.Translate(-(Time.deltaTime), 0f, 0f, Space.World);
+		if (Input.GetKey(KeyCode.RightArrow))
+			transform.Translate((Time.deltaTime), 0f, 0f, Space.World);
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			transform.Rotate(0f, 0f, (Time.deltaTime), Space.World);
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			transform.Rotate(0f, 0f, -(Time.deltaTime), Space.World);
+		}
+		//Debug.Log(transform.rotation.eulerAngles);
+#else
+		transform.Translate(-Input.acceleration.x * (Time.deltaTime * speed * 0.1f), 0f, 0f, Space.World);
+		transform.Rotate(0f, 0f, Input.acceleration.y * (Time.deltaTime * speed), Space.World);
+#endif
+		if (transform.rotation.eulerAngles.z > 180f) {
+			Quaternion rot = Quaternion.Euler(0f, 0f, 0f);
+			transform.rotation = rot;
+		}
+		else if (transform.rotation.eulerAngles.z > 90f) {
+			Quaternion rot = Quaternion.Euler(0f, 0f, 90f);
+			transform.rotation = rot;
+		}
+
+		speedUpMultiplier = Mathf.Abs(transform.rotation.eulerAngles.z / 30f) * 0.5f;
+		if (speedUpMultiplier < 0.5f)
+			speedUpMultiplier = 0.5f;
+	}
+
+	public void OnButton()
+	{
+		transform.position = initPos;
+		transform.rotation = initRot;
 	}
 }
