d73c470a9d1b7c2eac7815f3695c8afcccd950f6
Christopher George <SoccerGeorge10@gmail.com>
2017-01-21 00:18:40 -0600

    mobile test

---
+++ Assets/MoblieRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoblieRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (SystemInfo.supportsGyroscope)
+			transform.rotation = Input.gyro.attitude;
+		else
+			transform.rotation = Quaternion.Euler(Time.time * 10f, 0f, 0f);
+	}
+}
